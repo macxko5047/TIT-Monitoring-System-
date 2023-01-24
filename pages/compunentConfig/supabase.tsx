2c43d649202bff3cc363b1ba1ca414d5db3e962b
macxko5047 <78531428+macxko5047@users.noreply.github.com>
2023-01-24 13:03:34 +0700

    up success

--- pages/compunentConfig/supabase.tsx
+++ pages/compunentConfig/supabase.tsx
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl: any = "https://lldzljxhukgbylauxnvo.supabase.co";
-const supabaseKey: any =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsZHpsanhodWtnYnlsYXV4bnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAyOTkwMDIsImV4cCI6MTk4NTg3NTAwMn0.Lp4hg1lEBD7KYFckhxaG6NPGo5LSWngUBa37UDhkrNQ";
-const supabase = createClient(supabaseUrl, supabaseKey);
-console.log(supabase);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
