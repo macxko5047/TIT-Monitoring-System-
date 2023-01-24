ebb3bcb1cec5cd7e7838a021adb706901cd0e24c
macxko5047 <78531428+macxko5047@users.noreply.github.com>
2023-01-24 10:36:24 +0700

    .gitignore fix

--- pages/compunentConfig/supabase.tsx
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl:string = 'https://vgtgaezrhsndydtxwtjf.supabase.co'
-const supabaseKey:string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZndGdhZXpyaHNuZHlkdHh3dGpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkwMjMyMzcsImV4cCI6MTk4NDU5OTIzN30.6PADXcVh8DTTzcX8tugK28NBWkuCKE3uqShd_Ge0En0'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase;
